bceb3ec1d624ad2f2886137ae10f3be5b93feef5
Philippe Vienne <philippegeek@gmail.com>
2017-02-27 22:25:51 +0100

    Revert "List containers and get data about"
    This reverts commit 234bf6905fc742bc91352ba80474833f4d05da5a.
    
    	deleted:    account/templates/manager/container.html
    	deleted:    account/templates/manager/containers.html
    	deleted:    account/templates/manager/utils/status_badge.html
    	deleted:    manager/docker.py
    	deleted:    manager/urls.py
    	modified:   manager/views.py
    	modified:   prodmanager/urls.py
    	deleted:    static/js/tooltips.js
    	modified:   templates/base.html
    	modified:   templates/navbar.html

--- manager/views.py
+++ manager/views.py
@@ -1,23 +1,3 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic import TemplateView
 
-from manager.docker import DockerClient
-
-
-class ContainersView(LoginRequiredMixin, TemplateView):
-    template_name = 'manager/containers.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['containers'] = DockerClient().containers
-        return context
-
-
-class ContainerView(LoginRequiredMixin, TemplateView):
-    template_name = 'manager/container.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['container'] = DockerClient().get_container(kwargs['id'])
-        return context
+# Create your views here.
--- prodmanager/urls.py
+++ prodmanager/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('account.urls')),
-    url(r'^', include('manager.urls'))
+    url(r'^', include('account.urls'))
 ]
